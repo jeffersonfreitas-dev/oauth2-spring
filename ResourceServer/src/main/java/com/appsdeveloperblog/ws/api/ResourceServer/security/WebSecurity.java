1cb496c574a46a98b841f8cac924c5af77e00a13
jeffersonfreitas21 <jefferson.dev21@gmail.com>
2022-03-17 22:05:07 -0300

    Inicio config jwtConverter in configure Spring Security

--- ResourceServer/src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
+++ ResourceServer/src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/security/WebSecurity.java
@@ -4,9 +4,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter{
+	
+	//Registrar o KeycloakConverter no SpringSecurity utilizando a classe JwtAuthenticationConverter e adicionando no método configure
+	JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+	jwtAuthenticationConverter
+//	jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
