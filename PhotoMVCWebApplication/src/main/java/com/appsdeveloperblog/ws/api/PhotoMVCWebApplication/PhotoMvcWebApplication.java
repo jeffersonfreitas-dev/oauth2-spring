9a9440cdd94a31b6b392af07f676bde7449bfa45
jeffersonfreitas21 <jefferson.dev21@gmail.com>
2022-03-23 21:24:03 -0300

    Create project MVC test

---
+++ PhotoMVCWebApplication/src/main/java/com/appsdeveloperblog/ws/api/PhotoMVCWebApplication/PhotoMvcWebApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.ws.api.PhotoMVCWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoMvcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoMvcWebApplication.class, args);
+	}
+
+}
---
+++ PhotoMVCWebApplication/src/test/java/com/appsdeveloperblog/ws/api/PhotoMVCWebApplication/PhotoMvcWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.ws.api.PhotoMVCWebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoMvcWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
